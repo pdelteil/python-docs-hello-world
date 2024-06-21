d71eda99845527e934a8a642129ece76f0bed1fb
Philippe Delteil <pdelteil@gmail.com>
2024-06-21 02:03:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    html_content = """
+    <!DOCTYPE html>
+    <html>
+
+    <body>
+      
+        <!-- Takeover by pdelteil -->
+    </body>
+    </html>
+    """
+    return html_content
+
+if __name__ == "__main__":
+    app.run(debug=True)
